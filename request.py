0ff5d85842b886cc3e62cf0fc3966723819f719f
Kenny <kenny@Tiffanys-MacBook-Pro.local>
2022-06-11 16:37:31 -0600

    testing push

---
+++ request.py
@@ -0,0 +1,9 @@
+import pandas
+import requests
+import os
+
+class spotify_request:
+    def __init__(self):
+        CLIENT_ID = os.environ.get(Spotify_Client_ID)
+
+print(spotify_request.CLIENT_ID)
