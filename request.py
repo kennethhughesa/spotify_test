f5b6d1c657b53fdbf00f0768b063e285451fd6b4
Kenny <kenny@Tiffanys-MacBook-Pro.local>
2022-06-11 17:09:36 -0600

    updated request file

--- request.py
+++ request.py
@@ -1,9 +1,12 @@
-import pandas
+import pandas as pd
 import requests
 import os
 
 class spotify_request:
-    def __init__(self):
-        CLIENT_ID = os.environ.get(Spotify_Client_ID)
+    def __init__(self, client_id):
+        self.CLIENT_ID = os.environ.get(client_id)
 
-print(spotify_request.CLIENT_ID)
+
+client = spotify_request('Spotify_Client_ID')
+
+print(client.CLIENT_ID)
